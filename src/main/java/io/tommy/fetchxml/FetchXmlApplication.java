bc4d567217eac4ff68915bf2c01fbaa93b99ca8a
Balaji prabu <complanboy@gmail.com>
2019-10-28 11:50:43 +0530

    init commit

---
+++ src/main/java/io/tommy/fetchxml/FetchXmlApplication.java
@@ -0,0 +1,13 @@
+package io.tommy.fetchxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchXmlApplication.class, args);
+	}
+
+}
